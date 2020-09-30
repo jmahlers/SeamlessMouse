9fc7a5cbe2553514e74abbefdbf1c16960ada62a
Jeff Ahlers <49463671+jmahlers@users.noreply.github.com>
2020-09-30 01:40:57 -0500

    Project creation

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeamlessMouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
